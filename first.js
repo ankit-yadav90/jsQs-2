e52374591caa094461dfac771757d5885ff8d14c
ankit-yadav90 <ankityadav73699@gmail.com>
2025-03-11 20:56:10 +0530

    prompt use

---
+++ first.js
@@ -0,0 +1,9 @@
+//Conditinal Statement
+
+let num = prompt("enter a number:");
+
+if (num % 5 === 0) {
+    console.log(num, " is multipal of 5");
+} else {
+    console.log(num, " is Not a multipal of 5");
+}
